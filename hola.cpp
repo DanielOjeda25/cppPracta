1762f85ebf6a0e00bc33d1ae288645806510501f
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2023-09-04 12:38:34 -0300

    Probando la sobrecarga

--- hola.cpp
+++ hola.cpp
@@ -1,72 +1,36 @@
 #include <iostream>
 using namespace std;
 
-// Declaracion de la clase Dog
-class Dog
+class Point
 {
-  // atributos
-  string name;
-  int peso;
-  // constructor public co sus metodos
 public:
-  // constructor
-  Dog();
-  // setter
-  void setName(const std::string &namesDog);
-  void setPeso(int weight);
-  virtual void print() const;
-  void bark() const { std::cout << name << " Barks"; } //<-- Metodo o accion
-  virtual ~Dog();                                      //<-- Destructor
+  double x = 0;
+  double y = 0;
+  Point(){};
+
+  Point(double a, double b) : x(a),
+                              y(b) {}
+
+  Point operator+(const Point &rhs) const;
+  Point &operator+=(const Point &rhs);
 };
-// Asignacion de la clase
-Dog::Dog()
+
+Point Point::operator+(const Point &rhs) const
 {
-  std::cout << "un perro ha sido creado"
-            << "\n";
-}
-void Dog::setName(const std::string &namesDog)
-{
-  name = namesDog;
-}
-void Dog::setPeso(int weight)
-{
-  peso = weight;
-}
-void Dog::print() const
-{
-  std::cout << "El perro es " << name << " y pesa " << peso << "kg\n";
-}
-Dog::~Dog()
-{
-  std::cout << "Adios " << name << "\n";
+  return Point(x + rhs.x, y + rhs.y);
 }
 
-// // Herencia
-// class MiNoPerro : public Dog
-// {
-//   void setMio(const std::string &miPerro);
-//   void print() const override;
-
-// private:
-//   std::string owner;
-// };
-// void MiNoPerro::setMio(const std::string &miPerro)
-// {
-//   owner = miPerro;
-// };
-
-// void MiNoPerro::print() const
-// {
-//   Dog::print();
-//   std::cout << "El perro es de " << owner << "\n";
-// }
-
-int main(int argc, char const *argv[])
+Point &Point::operator+=(const Point &rhs)
 {
-  Dog myDog;
-  myDog.setName("PUTO PERRO");
-  myDog.setPeso(10);
-  myDog.print();
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
 
-  return 0;
-};
+int main()
+{
+  Point up(0, 1);
+  Point right(1, 0);
+  Point result = up + right;
+  cout << "Result is upright (" << result.x << ',' << result.y << ")\n";
+}
